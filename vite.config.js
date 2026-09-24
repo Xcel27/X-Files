359e41ae2d2fda537c200aa0d06ac5e3c20065c4
Excel Akpan <excelakpan@icloud.com>
2026-09-24 12:23:40 +0100

    Configure Vite for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/X-Files/',
   plugins: [react()],
 })
